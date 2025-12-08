fc5147c4bcd42cddb8a577dfa06b8f406640a6c8
ik0326 <ik0246game0246@gmail.com>
2025-12-08 22:53:50 +0900

    cryptoのrandomUUID関数を用いてuuidv4を生成

--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
 // UUID generator
+/**
+ * Web標準の crypto.randomUUID() を使用してUUID v4を生成
+ * (最新のブラウザ、Deno、Node.js >= 16で利用可能)
+ * @returns {string} UUID v4
+ */
 function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+  return crypto.randomUUID();
 }
 
 // DOM
